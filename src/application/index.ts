1bd3a7d7ffe7287ecae589df6359c52f9df25a55
Atikur Rahman <atikassam@outlook.com>
2018-04-23 11:56:18 +0530

    main.js updated

--- src/application/index.ts
+++ src/application/index.ts
@@ -2,4 +2,4 @@ const $ = require("jquery");
 
 // Lets change the text of poc container
 $('#poc').html("This is a simple poc for webpack")
-$('#sub').html("Webpack is successfully bundled all the dependences in one static file in main.js")
+$('#sub').html("Webpack is successfully bundled all the dependences in one static file, into main.js")
